7e725be68130000f2268736833e3ea1cb8c7dce6
Gino Messmer <git@ginomessmer.me>
2020-07-03 16:35:06 +0200

    Refactoring

--- CDMA/Cdma.cs
+++ CDMA/Cdma.cs
@@ -8,29 +8,22 @@ namespace CDMA
     {
         public static int[] Decode(int[] code, int[] payload)
         {
-            var decodedMessage = new List<int>();
-
             var allChunks = payload.ChunkBy(code.Length);
-            foreach (var chunk in allChunks)
-            {
-                var scalar = chunk.Select((i, index) => i * code[index]).Sum() / code.Length;
-                decodedMessage.Add(scalar);
-            }
 
-            return decodedMessage.ToArray();
+            return allChunks
+                .Select(chunk => 
+                    chunk.Select((i, index) => i * code[index]).Sum() / code.Length)
+                .ToArray();
         }
 
         public static int[] Encode(int[] code, int[] message)
         {
             var list = new List<int>();
 
-            for (var i = 0; i < message.Length; i++)
+            foreach (var currentMessage in message)
             {
-                var currentMessage = message[i];
-                for (var j = 0; j < code.Length; j++)
+                foreach (var currentCode in code)
                 {
-                    var currentCode = code[j];
-
                     if (currentMessage == 0)
                         list.Add(-currentCode);
                     else
@@ -46,13 +39,7 @@ namespace CDMA
             if (inputA.Length != inputB.Length)
                 throw new InvalidOperationException("Lists do not have the same size");
 
-            var newResult = new List<int>();
-            for (var i = 0; i < inputA.Length; i++)
-            {
-                newResult.Add(inputA[i] + inputB[i]);
-            }
-
-            return newResult.ToArray();
+            return inputA.Select((t, index) => t + inputB[index]).ToArray();
         }
 
         public static IEnumerable<int> Normalize(int[] input) => input.Select(i => i < 0 ? 0 : 1);
